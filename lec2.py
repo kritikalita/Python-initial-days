0f88a2434385952a88ad85c3d169f48555b17aa8
kritikalita <kritikalita7@gmail.com>
2023-06-23 01:36:32 +0530

    my first commit

---
+++ lec2.py
@@ -0,0 +1,12 @@
+#conditional statements
+a=int(input("Enter 1st no."))
+b=int(input("Enter 2nd no."))
+c=input("Enter operator")
+if c=="+":
+    print(a+b)
+elif c=="-":
+    print(a-b)
+elif c=="*":
+    print(a*b)
+elif c=="//":
+    print(a//b)
---
+++ yashSirTutorials1.py
@@ -0,0 +1,7 @@
+# a=str(input("Your name:"))
+# print(a)
+
+# a = int(input("enter your age"))
+# b = a + 10
+# print("your age after 10 years", b)
+
